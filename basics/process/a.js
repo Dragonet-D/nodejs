75d0ae9a7df22b5779aaf7cd0a519e83cac1ed50
Deng XiaoLong <Dragonet_d@163.com>
2018-10-04 20:54:00 +0800

    小龙的nodejs学习计划

---
+++ basics/process/a.js
@@ -0,0 +1,13 @@
+let a;
+let b;
+process.stdout.write("情输入a:");
+process.stdin.resume();
+process.stdin.on("data", (chunk) => {
+    if (!a) {
+        a = Number(chunk);
+        process.stdout.write("请输入b的值:");
+    } else {
+        b = Number(chunk);
+        process.stdout.write(a+b);
+    }
+});
