63e3332c9cbb593f3e7f86a1437bad76caeaf8fd
Deng XiaoLong <Dragonet_d@163.com>
2018-10-16 22:50:50 +0800

    xiao long in SG

--- basics/fs/a.js
+++ basics/fs/a.js
@@ -16,3 +16,25 @@ fs.stat('upload', (err, stats) => {
         console.log(stats.isDirectory())
     }
 })
+
+// 找出html 目录下面的所有目录 并打印出来
+
+fs.readdir('html', (err, files) => {
+    if (err) {
+        console.log(err)
+        return false
+    } else {
+        // 判断是目录还是文件
+        console.log(files)
+        for (let i = 0; i < files.length; i++) {
+            fs.stat('html/'+files[i], (error, stats) => {
+                // 循环判断是目录还是文件 --- 异步;
+                if (stats.isDirectory()) {
+                    console.log(files[i])
+                }
+            })
+        }
+    }
+})
+
+
