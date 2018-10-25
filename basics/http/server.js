994dc9ce5cc6c489f39d4c8f95a78c16e3bc668c
Deng XiaoLong <Dragonet_d@163.com>
2018-10-25 22:29:02 +0800

    use http package to create server

---
+++ basics/http/server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+  let pathname = req.url;
+  if(pathname === '/') {
+    pathname = '/index.html'
+  }
+  if (pathname !== '/favicon.ico') {
+    // 文件操作
+    fs.readFile(`static/${pathname}`, (err, result) => {
+      if (err) {
+        console.log(404);
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html;charset="utf-8"'});
+        res.write(result);
+        res.end();
+      }
+    })
+  }
+}).listen(8080);
