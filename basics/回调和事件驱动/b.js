49474399005d208d59346aa3b8254bb3ccc47565
Dragonet-D <Dragonet_D@163.com>
2019-12-10 11:00:53 +0800

    :heart: node js eventEmitter

--- basics/回调和事件驱动/b.js
+++ basics/回调和事件驱动/b.js
@@ -6,7 +6,7 @@ const EventEmitter = new events.EventEmitter();
 
 console.log(1);
 function getData(callBack) {
-  fs.readFile('a.json', (err, data) => {
+  fs.readFile('./a.json', (err, data) => {
     EventEmitter.emit('data', data);
   });
 }
@@ -14,5 +14,6 @@ getData();
 
 // 监听广播数据
 EventEmitter.on('data', (mime) => {
+  console.log(mime)
   console.log(mime.toString());
 });
