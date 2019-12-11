405d7f225c93183336ae7dc4a049c15f52025067
Dragonet-D <Dragonet_D@163.com>
2019-12-11 16:48:18 +0800

    :art: nodejs dns

---
+++ basics/dns/dns1.js
@@ -0,0 +1,16 @@
+const dns = require('dns');
+
+dns.resolve4('archive.org', (err, addresses) => {
+  if (err) throw err
+  console.log(`地址: ${JSON.stringify(addresses)}`)
+
+  addresses.forEach(item => {
+    dns.reverse(item, (err, hostnames) => {
+    if (err) {
+      throw err
+    }
+
+    console.log(`地址 ${item} 逆向解析到域名: ${JSON.stringify(hostnames)}`)
+    })
+  })
+})
